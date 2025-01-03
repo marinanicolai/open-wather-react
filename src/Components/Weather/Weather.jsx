989f58b1d21b1b8c5364e49dc1cb0c4f9d93c678
Marina Nicolai <nicolaimarina@gmail.com>
2025-01-03 00:33:16 -0500

    added the api to the app

---
+++ src/Components/Weather/Weather.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react'
+
+function Weather() {
+const apiKey = process.env.API_KEY;
+let location = 'New York';
+useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+         .then((response) => {response.json()
+         console.log(response); 
+         })
+         .then(data => {
+            console.log('Parsed Data: ', data);
+         })
+})
+  
+
+  return (
+    <div>
+      <p>this is weather component</p>
+    </div>
+  )
+}
+
+export default Weather
